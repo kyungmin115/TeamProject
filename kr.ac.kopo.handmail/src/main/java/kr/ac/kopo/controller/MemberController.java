7292c8e25339cfe8a8c44e8c69882d82dac58d55
ShinJH0621 <mythewin@naver.com>
2022-09-01 15:54:15 +0900

    base
    base

---
+++ kr.ac.kopo.handmail/src/main/java/kr/ac/kopo/controller/MemberController.java
@@ -0,0 +1,73 @@
+package kr.ac.kopo.controller;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import kr.ac.kopo.model.MemberVO;
+import kr.ac.kopo.service.MemberService;
+
+@Controller
+@RequestMapping(value="/member/")
+public class MemberController {
+
+	@Autowired
+	MemberService memberService;
+	
+	@Inject
+	BCryptPasswordEncoder pwdEncoder;
+	
+	@GetMapping("join.do")
+	public String joinForm() {
+		
+		return "member/MemAdd";
+	}
+	
+	@PostMapping("join.do")
+	public String join(MemberVO vo) throws Exception {
+		
+		String inputPass = vo.getMemPass();
+		String pwd = pwdEncoder.encode(inputPass);
+		vo.setMemPass(pwd);
+		
+		memberService.insertMem(vo);
+		
+		return "redirect:/";
+	}
+	
+	
+	@PostMapping("login.do")
+	public String login(MemberVO vo, HttpSession session, RedirectAttributes rttr ) throws Exception {
+		
+		session.getAttribute("member");
+		MemberVO login = memberService.login(vo);
+		boolean pwdMatch = pwdEncoder.matches(vo.getMemPass(), login.getMemPass());
+
+		if(login != null && pwdMatch == true) {
+			session.setAttribute("member", login);
+		} else {
+			session.setAttribute("member", null);
+			rttr.addFlashAttribute("msg", false);
+		}
+		
+		return "redirect:/";
+		
+	}
+	
+	@GetMapping("logout.do")
+	public String logout(HttpSession session) throws Exception {
+		session.invalidate();
+		return "redirect:/";
+	}
+	
+	
+	
+}
---
+++ kr.ac.kopo.handmail/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<html>
+<head>
+	<title>손.편.지</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#logoutBtn").on("click", function(){
+			location.href="/member/logout.do";
+		})
+		
+	})
+</script>
+<body>
+
+
+<a href="/member/join.do">회원가입</a>
+
+
+<form name='homeForm' method="post" action="/member/login.do">
+		<c:if test="${member == null}">
+			<div>
+				<label for="memId"></label>
+				<input type="text" id="memId" name="memId">
+			</div>
+			<div>
+				<label for="memPass"></label>
+				<input type="password" id="memPass" name="memPass">
+			</div>
+			<div>
+				<button type="submit">로그인</button>
+			</div>
+		</c:if>
+		<c:if test="${member != null }">
+			<div>
+				<p>${member.memName}님 환영 합니다.</p>
+				<button id="logoutBtn" type="button">로그아웃</button>
+			</div>
+		</c:if>
+		<c:if test="${msg == false}">
+			<p style="color: red;">로그인 실패! 아이디와 비밀번호 확인해주세요.</p>
+		</c:if>
+	</form>
+
+</body>
+</html>
