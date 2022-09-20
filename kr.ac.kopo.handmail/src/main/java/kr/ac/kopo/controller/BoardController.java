33e4ffbf5e479c0eefe6956bfac40403520fdc3d
kyungmin115 <remember4623@gmail.com>
2022-09-20 16:59:34 +0900

    9/20 게시판
    BoardDao 완료

--- kr.ac.kopo.handmail/src/main/java/kr/ac/kopo/controller/BoardController.java
+++ kr.ac.kopo.handmail/src/main/java/kr/ac/kopo/controller/BoardController.java
@@ -1,5 +1,15 @@
 package kr.ac.kopo.controller;
 
-public class BoardController {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.ac.kopo.model.SearchInfo;
+import kr.ac.kopo.service.BoardService;
+
+@Controller
+public class BoardController {
+	
 }
--- kr.ac.kopo.handmail/src/main/java/kr/ac/kopo/dao/BoardDao.java
+++ kr.ac.kopo.handmail/src/main/java/kr/ac/kopo/dao/BoardDao.java
@@ -1,5 +1,20 @@
 package kr.ac.kopo.dao;
 
-public interface BoardDao {
+import java.util.List;
 
+import kr.ac.kopo.model.BoardVO;
+import kr.ac.kopo.model.SearchInfo;
+
+public interface BoardDao {
+	
+	List<BoardVO> selectBoardList(SearchInfo info);
+
+	int insertBoard(BoardVO vo);
+
+	int delBoard(int bbsNo);
+
+	BoardVO selectBoard(int bbsNo);
+
+	int updateBoard(BoardVO vo);
+	
 }
